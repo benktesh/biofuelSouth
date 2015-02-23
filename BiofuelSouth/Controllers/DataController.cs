8b56742637b25d6ecb33c0d49c650ea6faff6b4b
benktesh <benktesh@yahoo.com>
2015-02-22 23:27:55 -0600

    adds logging to data module

--- BiofuelSouth/Controllers/DataController.cs
+++ BiofuelSouth/Controllers/DataController.cs
@@ -1,22 +1,38 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Reflection;
 using System.Web.Mvc;
 using BiofuelSouth.Models;
+using log4net;
 
 namespace BiofuelSouth.Controllers
 {
     public class DataController :Controller
     {
+        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
         public ActionResult  CountiesForState(string state)
         {
-            DatabaseContext db = new DatabaseContext();
-            var counties = db.County.Where(p => p.State == state);
-            var result = (from s in counties
-                select new
-                {
-                    id = s.CountyCode,
-                    name = s.Name
-                }).ToList();
-            return Json(result, JsonRequestBehavior.AllowGet);
+            
+            try
+            {
+                DatabaseContext db = new DatabaseContext();
+                var counties = db.County.Where(p => p.State == state);
+                var result = (from s in counties
+                    select new
+                    {
+                        id = s.CountyCode,
+                        name = s.Name
+                    }).ToList();
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception exception)
+            {
+                Log.Error("Error processing counties" + exception);
+            }
+            return null;
+
+
         }
     }
 }
