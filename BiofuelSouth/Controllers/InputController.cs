340fd9da9d9159f1b4cd7f8c517980fe5f026e0d
benktesh <benktesh@yahoo.com>
2015-03-15 11:54:51 -0500

    changes input controller

--- BiofuelSouth/Controllers/InputController.cs
+++ BiofuelSouth/Controllers/InputController.cs
@@ -35,25 +35,28 @@ namespace BiofuelSouth.Controllers
         //[HttpPost]
         public ActionResult Index(Input ip= null)
         {
+            ip = null;
+            Session["Input"] = ip; 
 
+            return RedirectToAction("General");
             //create a session variable
 
-            ModelState.Clear();
+            //ModelState.Clear();
 
-            if (ip == null || ip.ProjectLife == 0)
-            {
-                FillViewBag();
-                //ip = new Input();
-                //ip.General = new General();
-                //Session["Input"] = ip;
+            //if (ip == null || ip.ProjectLife == 0)
+            //{
+            //    FillViewBag();
+            //    //ip = new Input();
+            //    //ip.General = new General();
+            //    //Session["Input"] = ip;
 
-                //return View(ip);
-                return RedirectToAction("General");
-            }
-            else
-            {
-                Session["Input"] = ip; 
-            }
+            //    //return View(ip);
+            //    return RedirectToAction("General");
+            //}
+            //else
+            //{
+            //    Session["Input"] = ip;
+            //}
                 
             //TODO Do some thing here
             if (ip.Storage != null && ip.Storage.StorageTime > 0)
@@ -78,13 +81,19 @@ namespace BiofuelSouth.Controllers
         
         public ActionResult General(General general=null)
         {
+            Input ip = (Input)Session["Input"];
+            if (ip == null)
+            {
+                ModelState.Clear();
+                ip = new Input();
+            }
             
             if (general == null || general.State == null)
             {
+             
                 general = new General();
-                Input ip = (Input) Session["Input"];
-                if (ip == null)
-                    ip = new Input();
+            
+                    
                 ip.General = general;
                 Session["Input"] = ip; 
                 return View();
@@ -98,7 +107,7 @@ namespace BiofuelSouth.Controllers
                 else
                 {
                     ModelState.Clear();
-                    Input ip = (Input)Session["Input"];
+                    ip = (Input)Session["Input"];
                     ip.General = general;
                     Session["Input"] = ip;
                     return RedirectToAction("Storage");
@@ -121,114 +130,170 @@ namespace BiofuelSouth.Controllers
 
         }
 
-        [Obsolete]
-        public ActionResult Storage1()
-        {
-            FillViewBag();
-            var ip = TempData["input"] as Input;
-            if (ip != null && ip.State != null)
-            {
-                TempData.Keep();
-                return View(ip);
-            }
-            return RedirectToAction("Input");
-            
-        }
 
-        public ActionResult Financial(Input ip = null)
-        {
-            if (ip == null || ip.County == null)
-                return RedirectToAction("Index");
-            return RedirectToAction("Index");
 
-          //TODO if financial is not needed, redirect to index
-        }
+        //public ActionResult Financial(Input ip = null)
+        //{
+        //    if (ip == null || ip.County == null)
+        //        return RedirectToAction("Index");
+        //    return RedirectToAction("Index");
+
+        //  //TODO if financial is not needed, redirect to index
+        //}
 
         public ActionResult Storage(Storage storage=null)
         {
-
-            if (storage == null)
-            {
-               storage = new Storage();
-            }
             Input ip = (Input)Session["Input"];
             if (ip == null)
             {
                 return RedirectToAction("Index");
             }
+            ip.Storage = storage;
+            Session["Input"] = ip;
+
+            //If storage is not null or storage is skipped then go to financial steps
+
+            if (storage != null && (storage.RequireStorage != null)) //This is post back
+            {
+                if ((bool) !storage.RequireStorage)
+                {
+                    return RedirectToAction("Financial");
+                }
+
+                if ((bool) storage.RequireStorage && ModelState.IsValid)
+                {
+                    return RedirectToAction("Financial");
+                }
+                
+
+                return View(storage);
+
+               
+            }
+
+            
+           storage = new Storage();
+           
+          
             if (ip.Storage == null)
             {
                 ip.Storage = storage;
             }
-            ip.Storage = storage;
-            Session["Input"] = ip;
-
+           
+            ModelState.Clear();
             return View(storage);
 
 
-            if (ip == null || ip.General == null)
+            //if (ip == null || ip.General == null)
+            //{
+            //    return RedirectToAction("Index");
+            //}
+
+            //if (ip.Storage == null)
+            //{
+            //    ip.Storage = new Storage();
+            //    return View();
+            //}
+            
+
+
+            //if (ip == null || ip.County == null)
+                
+
+            //if (ip.Storage == null)
+            //{
+            //    ip.Storage = new Storage();
+            //    ModelState.Clear();
+
+            //}
+            //else
+            //{
+            //    if (!ModelState.IsValid)
+            //        return View(ip);
+
+            //}
+               
+                
+            //FillViewBag();
+            //if (ModelState.IsValid)
+            //{
+            //    if (ip.Storage.StorageTime > 0)
+            //        ip.Storage.RequireStorage = true;
+            //    ViewBag.Results = true;
+            //    PostSubmit(ip, "Annual Production");
+            //    ViewBag.input = ip;
+            //    TempData["input"] = ip;
+            //    TempData.Keep();
+            //}
+
+            //return View(ip);
+
+            //if (ip.State == null)
+            //{
+            //    return Index();
+                
+            //}
+
+            //if (ip.Storage == null)
+            //{
+            //    ip.Storage = new Storage {RequireStorage = false};
+                
+            //    return View(ip);
+            //}
+                
+            //if (ip.Storage.StorageTime > 0)
+            //    ip.Storage.RequireStorage = true;
+            
+            //ViewBag.Results = true;
+            //PostSubmit(ip, "Annual Production");
+            //ViewBag.input = ip;
+            //return View(ip);
+        }
+
+        public ActionResult Financial(Financial financial = null)
+        {
+            Input ip = (Input)Session["Input"];
+            if (ip == null)
             {
                 return RedirectToAction("Index");
             }
+            ip.Financial = financial;
+            Session["Input"] = ip;
 
-            if (ip.Storage == null)
+            //If finance is not null or finance is skipped then go to results
+
+            if (financial != null && (financial.RequireFinance != null)) //This is post back
             {
-                ip.Storage = new Storage();
-                return View();
+                if (((bool)!financial.RequireFinance) || ((bool)financial.RequireFinance && ModelState.IsValid))
+                {
+                    return RedirectToAction("Result", ip);
+                }
+
+
+                return View(financial);
+
+
             }
+
+    
+
+            if (financial == null)
+            {
+                financial = new Financial();
+            }
+
+            if (ip.Financial == null)
+            {
+                ip.Financial = financial;
+            }
+
+            ModelState.Clear();
+            return View(financial);
             
+        }
 
-
-            if (ip == null || ip.County == null)
-                
-
-            if (ip.Storage == null)
-            {
-                ip.Storage = new Storage();
-                ModelState.Clear();
-
-            }
-            else
-            {
-                if (!ModelState.IsValid)
-                    return View(ip);
-
-            }
-               
-                
-            FillViewBag();
-            if (ModelState.IsValid)
-            {
-                if (ip.Storage.StorageTime > 0)
-                    ip.Storage.RequireStorage = true;
-                ViewBag.Results = true;
-                PostSubmit(ip, "Annual Production");
-                ViewBag.input = ip;
-                TempData["input"] = ip;
-                TempData.Keep();
-            }
-
-            return View(ip);
-
-            if (ip.State == null)
-            {
-                return Index();
-                
-            }
-
-            if (ip.Storage == null)
-            {
-                ip.Storage = new Storage {RequireStorage = false};
-                
-                return View(ip);
-            }
-                
-            if (ip.Storage.StorageTime > 0)
-                ip.Storage.RequireStorage = true;
-            
-            ViewBag.Results = true;
-            PostSubmit(ip, "Annual Production");
-            ViewBag.input = ip;
+        public ActionResult Result(Input ip)
+        {
             return View(ip);
         }
 
