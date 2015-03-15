b8e038a7b2fe8a0de487d552ff86acb2cdef3107
benktesh <benktesh@yahoo.com>
2015-03-14 22:54:36 -0500

    starts a separate financial model

--- BiofuelSouth/Models/Financial.cs
+++ BiofuelSouth/Models/Financial.cs
@@ -8,28 +8,28 @@ namespace BiofuelSouth.Models
 {
     public class Financial
     {
-        [DisplayName("Financial Interest Rate(%)")]
+        [DisplayName(@"Financial Interest Rate(%)")]
         public double InterestRate { get; set; }
 
 
-        [DisplayName("Annual Administrative Cost($/acre/year)")]
+        [DisplayName(@"Annual Administrative Cost($/acre/year)")]
         public double AdministrativeCost { get; set; } //$/acre/year
 
-        [DisplayName("Incentive Payment($/acre/year)")]
+        [DisplayName(@"Incentive Payment($/acre/year)")]
         public double IncentivePayment { get; set; } //$/acre/year
 
-        [DisplayName("Years of Incentive Payment (i.e., number of years)")]
+        [DisplayName(@"Years of Incentive Payment (i.e., number of years)")]
         public int YearsOfIncentivePayment { get; set; } //$acres/yearC:\Users\Benktesh\Documents\Visual Studio 2013\Projects\BiofuelSouthSolution\BiofuelSouth\Models\Input.cs
 
 
 
-        [DisplayName("Available Equity ($)")]
+        [DisplayName(@"Available Equity ($)")]
         public double AvailableEquity { get; set; } //$
 
-        [DisplayName("Loan Amount ($) (expected or current)")]
+        [DisplayName(@"Loan Amount ($) (expected or current)")]
         public double LoanAmount { get; set; } //$
 
-        [DisplayName("Equity Loan Interest Rate (% e.g. 4.05)")]
+        [DisplayName(@"Equity Loan Interest Rate (% e.g. 4.05)")]
         public double EquityLoanInterestRate { get; set; } //% (decimal fraction)
     }
 }
