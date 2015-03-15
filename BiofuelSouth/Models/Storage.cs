c2724a1e95a967f952711a24cd007c10d4741662
benktesh <benktesh@yahoo.com>
2015-03-14 22:54:24 -0500

    Splits storage as separate model

--- BiofuelSouth/Models/Storage.cs
+++ BiofuelSouth/Models/Storage.cs
@@ -18,15 +18,15 @@ namespace BiofuelSouth.Models
         public bool RequireStorage { get; set; } //true if storage is required, false if not.
 
         [Range(0, 200)]
-        [DisplayName("Storage Time in Days (Upto 200 days)")]
+        [DisplayName(@"Storage Time in Days (Upto 200 days)")]
         public Double StorageTime { get; set; } //number of days, Max 200 days;
         [Range(0,100)]
-        [DisplayName("Percentage Going Directly to Plantgate")]
+        [DisplayName(@"Percentage Going Directly to Plantgate")]
         public Double PercentDirectlyToPlantGate { get; set; }
         [Range(0, 100)]
-        [DisplayName("Percentage Requiring Storage")]
+        [DisplayName(@"Percentage Requiring Storage")]
         public Double PercentStored { get; set; } //proportion requiring storage
-         [DisplayName("Storage Method")]
+         [DisplayName(@"Storage Method")]
         public String StorageMethod { get; set; } //Storage methods.
     }
 }
