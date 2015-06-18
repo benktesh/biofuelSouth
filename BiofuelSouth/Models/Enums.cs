3c626e458b7201ce1277de1634ca1ebdf79d9623
benktesh <benktesh@yahoo.com>
2015-06-18 07:41:14 -0500

    adding croptype and storage selection enum

---
+++ BiofuelSouth/Models/Enums.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BiofuelSouth.Models
+{
+    public enum CropType
+    {
+        Switchgrass,
+        Miscanthus,
+        YelloPoplar,
+        Willow
+    }
+
+    public enum CostEstimationOption
+    {
+        Default = 0,
+        UserSupplyStorageCost = 1,
+        UserSupplyMaterialCost = 2
+    }
+}
