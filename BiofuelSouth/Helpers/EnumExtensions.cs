3c7cb71450a9b61354605187a7f5645bec96477d
benktesh <benktesh@yahoo.com>
2015-12-23 16:57:54 -0600

    adds EnumExtensions

---
+++ BiofuelSouth/Helpers/EnumExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+namespace BiofuelSouth.Helpers
+{
+    public static class EnumExtensions
+    {
+        public static string GetDisplayName(this System.Enum enumValue)
+        {
+            var attrs = enumValue.GetType()
+                .GetMember(enumValue.ToString())
+                .FirstOrDefault().GetCustomAttributes<DisplayAttribute>().FirstOrDefault();
+
+            if (attrs == null)
+            {
+                return enumValue.ToString();
+            }
+
+            return attrs
+                .GetName();
+
+    
+
+        }
+
+        public static string StringValue(this System.Enum value)
+        {
+            if (value == null)
+            {
+                return String.Empty;
+            }
+
+            var fi = value.GetType().GetField(value.ToString());
+            var attrs = fi.GetCustomAttributes(typeof (DisplayAttribute), false) as DisplayAttribute[];
+
+            if (attrs != null && attrs.Length > 0)
+            {
+                return attrs[0].Name;
+            }
+
+            return value.ToString();
+        }
+    }
+}
