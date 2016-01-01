4f21a878fad5a226e33c40ffa183f0838206f2d2
benktesh <benktesh@yahoo.com>
2016-01-01 10:36:17 -0600

    Created a LineGraph Generator in Chart Controller

--- BiofuelSouth/Controllers/ChartController.cs
+++ BiofuelSouth/Controllers/ChartController.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 using BiofuelSouth.Models;
@@ -9,8 +11,8 @@ namespace BiofuelSouth.Controllers
 {
     public class ChartController : Controller
     {
-        private int ChartCacheMinute = 5; 
-      public void GenerateChart(string cacheKey, Double[] data, String chartName)
+        private int ChartCacheMinute = 5;
+        public void GenerateChart(string cacheKey, decimal[] data, String chartName)
         {
             var xValues = Enumerable.Range(1, data.Length).ToArray();
             var chart = new Chart(600, 300);
@@ -25,7 +27,7 @@ namespace BiofuelSouth.Controllers
             chart.SaveToCache(cacheKey, ChartCacheMinute);
         }
 
-        public void GenerateColumnChart(string cacheKey, Double[] data, String chartName, string xLabel, string yLabel)
+        public void GenerateColumnChart(string cacheKey, decimal[] data, String chartName, string xLabel, string yLabel)
         {
             var xValues = Enumerable.Range(1, data.Length).ToArray();
             var chart = new Chart(600, 300);
@@ -33,12 +35,12 @@ namespace BiofuelSouth.Controllers
                 chartType: "Column",
                 name: chartName,
                 xValue: xValues,
-                
+
             yValues: data);
 
             chart.AddSeries(chartType: "line", yValues: Enumerable.Repeat(0, data.Length).ToArray());
             chart.SetXAxis(xLabel + " ", 0, data.Length + .75);
-            
+
             chart.AddTitle(chartName);
             //chart.SetYAxis(yLabel + " ", Math.Round(data.Min(),0), Math.Round(data.Max(),0));
             chart.SaveToCache(cacheKey, ChartCacheMinute);
@@ -46,7 +48,7 @@ namespace BiofuelSouth.Controllers
 
         public void GenerateCostRevenueChart(String cacheKey, Input ip, String chartName)
         {
-        
+
             var data = ip.GetRevenues().Select(m => m.TotalRevenue).ToArray();
             var xValues = Enumerable.Range(1, data.Length).ToArray();
 
@@ -56,13 +58,13 @@ namespace BiofuelSouth.Controllers
                 name: "Revenue",
                 xValue: xValues,
                 yValues: data);
-            
+
             data = ip.GetExpenditures().Select(m => m.TotalExpenses).ToArray();
             chart.AddSeries(
                 chartType: "Line",
-                
+
                 name: "Cost",
-                
+
                 xValue: xValues,
                 yValues: data);
 
@@ -71,6 +73,46 @@ namespace BiofuelSouth.Controllers
             chart.SetXAxis("Year", 0, data.Length + .75);
             chart.AddTitle(chartName);
             chart.SetYAxis("$ ");
+            chart.SaveToCache(cacheKey, ChartCacheMinute);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="cacheKey"></param>
+        /// <param name="dataMember"></param>
+        /// <param name="labels">Label corresponding to series names for datamember</param>
+        /// <param name="chartName"></param>
+        /// <param name="yLabel"></param>
+        /// <param name="xLabel"></param>
+        public void GenerateLineGraphs(String cacheKey, List<List<decimal>> dataMember, 
+            List<string> labels, string chartName, string yLabel = "$ ", string xLabel = "Year")
+        {
+            yLabel = yLabel + " ";
+            var dataItems = dataMember.Count;
+            if (dataItems == 0)
+                return; 
+
+            var xAxisLength = dataMember.First().Count;
+            if (xAxisLength == 0)
+                return;
+
+            var chart = new Chart(600, 300);
+            var xValues = Enumerable.Range(1, xAxisLength).ToArray();
+            for (int i = 0; i < dataItems; i++)
+            {
+                var data = dataMember[i].ToArray();
+                chart.AddSeries(
+                    chartType: "Line",
+                    name: labels[i],
+                    xValue: xValues,
+                    yValues: data);
+            }
+            
+            chart.AddLegend("Legend");
+            chart.SetXAxis(xLabel, 0, xAxisLength + .75);
+            chart.AddTitle(chartName);
+            chart.SetYAxis(yLabel);
             chart.SaveToCache(cacheKey, ChartCacheMinute);
         }
 
